9997e433113b60d8e1f92c6c6ed9a962a591ec8b
Akhand-Jyoti-Raj <rahulkumar108642@gmail.com>
2024-09-07 23:18:20 +0530

    push

---
+++ halfpriyamid.java
@@ -0,0 +1,12 @@
+public class halfpriyamid {
+    public static void main(String args[]) {
+        char ch = 'a';
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
