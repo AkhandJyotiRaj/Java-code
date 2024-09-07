f17b19920dfc8a168d32c2829da437d321fd6359
Akhand-Jyoti-Raj <rahulkumar108642@gmail.com>
2024-09-07 20:04:02 +0530

    new

---
+++ count.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+import javax.sound.midi.SysexMessage;
+public class count {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = 0;
+        while (b <= a) {
+            System.out.println(b);
+            b++;
+        }
+        
+    }
+}
---
+++ printlargest.java
@@ -0,0 +1,13 @@
+public class printlargest {
+    public static void main(String args[]) {
+        int a = 1;
+        int b = 3;
+
+        if (a > b) {
+            System.out.print("a is greater");
+        } else {
+            System.out.print("b is greater");
+        }
+
+    }
+}
