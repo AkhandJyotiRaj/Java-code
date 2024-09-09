cb2e326358856a3e7907eece9868a639794523d4
Akhand-Jyoti-Raj <rahulkumar108642@gmail.com>
2024-09-09 23:16:14 +0530

    push

---
+++ function.java
@@ -0,0 +1,12 @@
+public class function {
+    
+    public static void akhand() {
+        System.out.print("hwll ow sfkljsgandi");
+    }
+
+    public static void main(String args[]) {
+        
+        akhand();
+
+    }
+}
