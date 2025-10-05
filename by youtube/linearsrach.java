ff036ce866850f1c142f0dfc1f4a2d6bc5cbfc32
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-10-05 09:33:46 +0530

    commit

---
+++ by youtube/linearsrach.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class linearsrach {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array ");
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        int[] arr = new int[n];
+
+
+        System.out.println("enter all element " + n + " element");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                System.out.println(arr[i]);
+                break;
+            }
+        }
+        
+    }
+}
