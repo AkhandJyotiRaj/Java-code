5915f7f1ecb01d82976ab0ab77b8c7655046eef2
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-10-09 23:02:52 +0530

    add

--- by youtube/new_recurssion/printnumber.java
+++ by youtube/new_recurssion/printnumber.java
@@ -4,25 +4,35 @@ public class printnumber {
 
     static int n;
     static int x = 1;
-    
-    public static void printt(int x){
 
-        if (x>n) {
+    // public static void printt(int x){
+
+    //     if (x>n) {
+    //         return;
+    //     }
+    //     System.out.print(x + " ");
+
+    //     printt(x+1);
+    // }
+
+    public static void printt(int n){
+
+        if (n == 0) {
+           
             return;
         }
-        System.out.print(x + " ");
-
-        printt(x+1);
+        // System.out.print(n + " ");
+        printt(n - 1);
+        System.out.print((n) + " ");
     }
+        public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter your number");
         n = sc.nextInt();
 
-        printt(x);
-        
+        printt(n);
+
     }
 }
