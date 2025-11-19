1a7fb23fc1a819fcba97497536c3a6edfee02b6a
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-11-19 12:02:44 +0530

    using swap

--- Code Force/19-11-25 recursion/LeetReversestring.java
+++ Code Force/19-11-25 recursion/LeetReversestring.java
@@ -16,3 +16,23 @@ class Solution {
         }
     }
 }
+
+// recursion method
+
+class Solution {
+    public void reverseString(char[] s) {
+        helper(s,0,s.length-1);
+          
+    }
+
+    public static void helper(char[] s,int st,int end){
+        if(st>=end) return;
+
+        char temp = s[st];
+        s[st]= s[end];
+        s[end] = temp;
+        helper(s,st + 1,end - 1);
+    }
+    
+
+}
