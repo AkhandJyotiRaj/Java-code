5617912c79a9adc209909155d69cca610de1b000
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-11-21 05:20:37 +0530

    binary Search

---
+++ by youtube/Seaching & Sorting/BinarSearchh.java
@@ -0,0 +1,30 @@
+
+public class BinarSearchh {
+    
+    public static void serachh(int[] arr, int target) {
+        
+        int st = 0;
+        int end = arr.length - 1;
+
+        while (st <= end) {
+            int mid = (st + end) / 2;
+
+            if (target == arr[mid]) {
+                System.out.println("find  index " + mid);
+                break;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            }
+            else
+                st = mid + 1;
+        }
+            
+        }
+
+    public static void main(String[] args) {
+        
+        int[] arr = { 2, 4, 6, 8, 9, 10 };
+        serachh(arr,10);
+
+    }
+}
