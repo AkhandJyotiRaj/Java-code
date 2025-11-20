7caf8e6231f30c1287ec384a3687a11d196eba90
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-11-21 03:26:40 +0530

    array list

---
+++ by youtube/ArrayList/Arraylisttt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Arraylisttt {
+    
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> l1 = new ArrayList<>();
+
+        l1.add(5);
+        
+    }
+}
