defb83e0d9d260f95a5f9d65c6c606f0ee537dce
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-11-20 11:46:59 +0530

    tower of hanoi

---
+++ Code Force/20-11-25 Linkedlist/TowerOfHanoi.java
@@ -0,0 +1,24 @@
+public class TowerOfHanoi {
+
+    public static void solve(int n, char fromRod, char toRod, char auxRod) {
+        // Base case
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + fromRod + " to " + toRod);
+            return;
+        }
+
+        // Move top n-1 disks from source to auxiliary
+        solve(n - 1, fromRod, auxRod, toRod);
+
+        // Move the nth disk from source to destination
+        System.out.println("Move disk " + n + " from " + fromRod + " to " + toRod);
+
+        // Move the n-1 disks from auxiliary to destination
+        solve(n - 1, auxRod, toRod, fromRod);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;  // number of disks
+        solve(n, 'A', 'C', 'B');  // A = source, C = destination, B = auxiliary
+    }
+}
