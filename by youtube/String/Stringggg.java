22cf8a8cd6787811c68d1dd585a033c05b7736a9
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2025-11-21 04:38:04 +0530

    sTring

---
+++ by youtube/String/Stringggg.java
@@ -0,0 +1,14 @@
+public class Stringggg {
+    
+    public static void main(String[] args) {
+        
+        String name = "akhand";
+        // System.out.println(name);
+
+        int a = 5;
+
+        System.out.printf("My name is %S and my age is %d", name, a);
+        System.out.println();
+        System.out.println(name.length());
+    }
+}
