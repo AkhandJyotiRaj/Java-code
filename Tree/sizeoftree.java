59f71e129d88a114d6ea148d7a6cfdea9811c85e
Akhand Jyoti Raj <rahulkumar108642@gmail.com>
2026-05-09 23:23:44 +0530

    max size

--- Tree/sizeoftree.java
+++ Tree/sizeoftree.java
@@ -13,6 +13,26 @@ public class sizeoftree {
         }
     }
 
+    public static int size(Node root) {
+        if (root == null)
+            return 0;
+        return 1 + size(root.left) + size(root.right);
+    }
+
+    public static int sumof(Node root) {
+        if (root == null)
+            return 0;
+
+        return root.val + sumof(root.left) + sumof(root.right);
+
+    }
+
+    public static int maxval(Node root) {
+        if (root == null)
+            return Integer.MIN_VALUE;
+        return Math.max(root.val, Math.max(maxval(root.left), maxval(root.right)));
+    }
+
     public static void preorder(Node root) {
         if (root == null)
             return;
@@ -20,6 +40,7 @@ public class sizeoftree {
         preorder(root.left);
         preorder(root.right);
     }
+
     public static void main(String[] args) {
         Node root = new Node(5);
         Node a = new Node(1);
@@ -30,11 +51,16 @@ public class sizeoftree {
         Node d = new Node(4);
         a.left = c;
         a.right = d;
-        Node e = new Node(5);
+        Node e = new Node(50);
         Node f = new Node(6);
         b.left = e;
         b.right = f;
         // display(root);
-        preorder(root);
+        // preorder(root);
+        // System.out.println(size(root));
+        System.out.println(sumof(root));
+        System.out.println(maxval(root));
     }
 }
+
+1:21:08 time span
